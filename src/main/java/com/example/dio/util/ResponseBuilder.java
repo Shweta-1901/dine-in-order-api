f71e44c8c9738703856106a2930ac2974d62e7e0
Shweta-1901 <Nandi@191>
2025-02-27 17:11:53 +0530

    ResponseBuilder Modified

--- src/main/java/com/example/dio/util/ResponseBuilder.java
+++ src/main/java/com/example/dio/util/ResponseBuilder.java
@@ -1,4 +1,41 @@
 package com.example.dio.util;
 
+import com.example.dio.model.User;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public class ResponseBuilder {
+
+    //esponseStructure<User> structure= new ResponseStructure<>();
+    //        structure.setData(user);
+//        structure.setStatus(HttpStatus.CREATED.value());
+//        structure.setMessage("user created");
+//ResponseStructure<User> responseStructure=ResponseStructure.<User>builder().status(HttpStatus.CREATED.value()).message("User Created").data(user).build();
+//return ResponseEntity.status(HttpStatus.CREATED).body(responseStructure);
+    public static <T>ResponseEntity<ResponseStructure<T>> success(HttpStatus httpStatus,String message,T data)
+    {
+        ResponseStructure<T> structure=  ResponseStructure.<T>builder()
+                .status(httpStatus.value())
+                .message(message)
+           .data(data)
+                .build();
+                return ResponseEntity.status(HttpStatus.CREATED)
+                        .body(structure);
+
+    }
+//    public static <T>ResponseEntity<ResponseStructure<T>> success(HttpStatus httpStatus, HttpHeaders header, String message, T data)
+//    {
+//        ResponseStructure<T> structure=  ResponseStructure.<T>builder()
+//                .status(httpStatus.value())
+//                .message(message)
+//                .data(data)
+//                .build();
+//        return ResponseEntity.status(HttpStatus.CREATED)
+//                .header(header)
+//                .body(structure);
+//
+//    }
+
+
 }
