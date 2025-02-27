72777f29a4781ae00b4301293ab9cc5a2cd2ce13
Shweta-1901 <Nandi@191>
2025-02-27 17:12:42 +0530

    UserServiceImpl modified

--- src/main/java/com/example/dio/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/dio/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.dio.service.impl;
 
+import com.example.dio.model.Admin;
+import com.example.dio.model.Staff;
+import com.example.dio.model.User;
 import com.example.dio.repository.UserRepository;
 import com.example.dio.service.UserService;
 import lombok.AllArgsConstructor;
-import model.User;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,7 +15,32 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User registerUser(User user){
-        return userRepository.save(user);
+
+        User user2= this.getUser(user);
+
+        return userRepository.save(user2);
+    }
+
+    private User getUser(User user) {
+        User user2;
+
+        switch (user.getUserRole()){
+            case Admin -> user2=new Admin();
+            case Staff -> user2 = new Staff();
+            default -> throw new RuntimeException("Failed to register");
+
+        }
+        return user2;
+    }
+
+    private  void mapToNewUser(User user, User user2){
+        user2.setUserId(user.getUserId());
+        user2.setUserName(user.getUserName());
+        user2.setUserRole(user.getUserRole());
+        user2.setEmail(user.getEmail());
+        user2.setPhoneNo(user.getPhoneNo());
+        user2.setPassword(user.getPassword());
+
     }
 
 
