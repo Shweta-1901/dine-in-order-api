e5c82f47fac8584d65810188d1a340eb029e5741
Shweta-1901 <Nandi@191>
2025-02-27 22:41:48 +0530

    modified

--- src/main/java/com/example/dio/repository/UserRepository.java
+++ src/main/java/com/example/dio/repository/UserRepository.java
@@ -1,8 +1,15 @@
 package com.example.dio.repository;
 
-import model.User;
+import com.example.dio.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Override
+    Optional<User> findById(Long userId);
+
 
 }
