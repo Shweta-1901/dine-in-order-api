ff2bd6b1005c9decb4dd1b0aca41270cd1c8fd56
Shweta-1901 <Nandi@191>
2025-02-27 22:42:37 +0530

    modifying

--- src/main/java/com/example/dio/exceptions/UserNotFoundByIdException.java
+++ src/main/java/com/example/dio/exceptions/UserNotFoundByIdException.java
@@ -1,4 +1,11 @@
 package com.example.dio.exceptions;
 
-public class UserNotFoundByIdException {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserNotFoundByIdException extends RuntimeException{
+    private final String message;
+
 }
