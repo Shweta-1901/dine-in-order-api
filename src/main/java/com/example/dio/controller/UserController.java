f9926ec6c5600750d88227408238445f49f65599
Shweta-1901 <Nandi@191>
2025-02-27 16:45:57 +0530

    UserController modified

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.dio.controller;
 
 import com.example.dio.model.User;
 import com.example.dio.service.UserService;
+import com.example.dio.util.ResponseBuilder;
 import com.example.dio.util.ResponseStructure;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -18,17 +19,18 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user){
+    public ResponseEntity<ResponseStructure<User>> registerUser(@RequestBody User user) {
         user = userService.registerUser(user);
+    return ResponseBuilder.success(HttpStatus.CREATED, "User Created",user);
+    }
 
 // ResponseStructure<User> structure= new ResponseStructure<>();
 //        structure.setData(user);
 //        structure.setStatus(HttpStatus.CREATED.value());
 //        structure.setMessage("user created");
-ResponseStructure<User> responseStructure=ResponseStructure.<User>builder().status(HttpStatus.CREATED.value()).message("User Created").data(user).build();
+//ResponseStructure<User> responseStructure=ResponseStructure.<User>builder().status(HttpStatus.CREATED.value()).message("User Created").data(user).build();
+//return ResponseEntity.status(HttpStatus.CREATED).body(responseStructure);
 
-return ResponseEntity.status(HttpStatus.CREATED).body(responseStructure);
 
-    }
 
 }
