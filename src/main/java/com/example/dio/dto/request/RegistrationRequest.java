cf053a71829fcba79795aa27f65eaacf4a879f3b
Shweta-1901 <Nandi@191>
2025-02-28 16:39:24 +0530

    modifying

--- src/main/java/com/example/dio/dto/request/RegistrationRequest.java
+++ src/main/java/com/example/dio/dto/request/RegistrationRequest.java
@@ -1,4 +1,16 @@
 package com.example.dio.dto.request;
 
+import com.example.dio.enums.UserRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class RegistrationRequest {
+    private String userName;
+    private String email;
+    private String password;
+    private String phoneNo;
+    private UserRole userRole;
 }
