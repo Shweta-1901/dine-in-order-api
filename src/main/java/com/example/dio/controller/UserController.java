8c11a36a2ad0cf7b653d55a34530b06448b9e876
Shweta-1901 <Nandi@191>
2025-03-01 15:12:21 +0530

    modifying

--- src/main/java/com/example/dio/controller/UserController.java
+++ src/main/java/com/example/dio/controller/UserController.java
@@ -8,6 +8,7 @@ import com.example.dio.model.User;
 import com.example.dio.service.UserService;
 import com.example.dio.util.ResponseBuilder;
 import com.example.dio.util.ResponseStructure;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,7 +24,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<ResponseStructure<UserResponse>> registerUser(@RequestBody RegistrationRequest registrationRequest) {
+    public ResponseEntity<ResponseStructure<UserResponse>> registerUser(@RequestBody @Valid RegistrationRequest registrationRequest) {
       UserResponse  user = userService.registerUser(registrationRequest);
     return ResponseBuilder.success(HttpStatus.CREATED, "User Created",user);
     }
@@ -43,7 +44,7 @@ public class UserController {
         return ResponseBuilder.success(HttpStatus.OK, "User Found",user);
     }
 @PutMapping("/users/{userId}")
-    public ResponseEntity<ResponseStructure<UserResponse>> updateUserById( @PathVariable long userId, @RequestBody UserRequest userRequest){
+    public ResponseEntity<ResponseStructure<UserResponse>> updateUserById( @PathVariable long userId, @RequestBody  UserRequest userRequest){
         UserResponse userResponse = userService.updateUserNameById(userRequest,userId);
         return ResponseBuilder.success(HttpStatus.OK,"user not updated",userResponse);
 
